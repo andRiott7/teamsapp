daa9ebd37e10f4e6f899d5f139da20a03d4b0aac
Andriotti <andriott7@gmail.com>
2024-02-12 09:40:06 -0300

    component for group cards

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
 import { Container } from './styles';
 import { Header } from '@components/Header';
-
+import { Highlight } from '@components/Hightlight';
+import { GroupCard } from './../../components/GroupCard/index';
 export function Groups() {
   return (
     <Container>
       <Header showBackButton />
+      <Highlight
+        title="Team"
+        subtitle="Work with your teams"
+      />
+      <GroupCard title="Team Members" />
     </Container>
   );
 }
