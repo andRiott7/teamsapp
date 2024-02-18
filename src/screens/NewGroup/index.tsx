03e0d9ff54b67af4fa9f1ab8ecff4d825de04490
Andriotti <andriott7@gmail.com>
2024-02-18 14:20:11 -0300

    local storage for groups

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,13 +6,31 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
+import { groupCreate } from "@storage/group/groupsCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
   const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate('players', { group });
+  async function handleNew() {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert('New Group', 'Enter the group name')
+      }
+
+      await groupCreate(group);
+      navigation.navigate('players', { group });
+
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("New Group", error.message);
+      } else {
+        Alert.alert("New Group", "Error: Unable to create a new group");
+        console.log(error);
+      }
+    }
   }
   return (
     <Container>
